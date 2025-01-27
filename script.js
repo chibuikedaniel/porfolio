aa016ec88b89f1ac7a2ae272e62b90d4c2ccbea2
dracuzi <danielchibuike77@gmail.com>
2025-01-27 16:50:17 +0100

    add Swiper.js functionality for my tech stack

---
+++ script.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.swiper', {
+    loop: true, // Enable infinite loop
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    slidesPerView: 4,
+    spaceBetween: 0,
+    
+});
